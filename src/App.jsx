3e790cf28ac657dcbd5ba41966220b475ecc1488
Lucas Weidas <lucasweidas@gmail.com>
2023-03-19 18:43:08 -0300

    Add jsx of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import Header from './components/header';
+import Main from './components/main';
+import Footer from './components/footer';
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
